df22f95aae83c2b7446652a2205a838cd3eb5509
Karim Soliman <karim.soliman@pua.edu.eg>
2021-07-22 02:22:29 +0200

    New Updates

---
+++ resources/js/Views/Admin/AddInv.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container>
+        <v-row justify="space-around">
+            <v-col cols="12" lg="5" md="5">
+                <v-card color="grey lighten-5">
+                    <v-card-title>
+                        <h1 class="text-h4 font-weight-light">Add new inv</h1>
+                    </v-card-title>
+                    <v-card-text>
+                        
+                    </v-card-text>
+                </v-card>
+                
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+
+    
+}
+</script>
