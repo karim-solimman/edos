76164e1456a760aa18dd6ef747ff587b14f48fed
Karim Soliman <karim.soliman@pua.edu.eg>
2021-08-12 19:07:24 +0200

    new Updates

--- resources/js/components/NavigationDrawer.vue
+++ resources/js/components/NavigationDrawer.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-navigation-drawer v-model="drawer" app>
+    <v-navigation-drawer disable-resize-watcher v-model="drawer" app>
             <v-list dense>
                 <v-list-item>
                     <v-list-item-content>
