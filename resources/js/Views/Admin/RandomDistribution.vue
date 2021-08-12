300bf91fdbcdc12fd20d6c0071a201c0e7bb34e2
Karim Soliman <karim.soliman@pua.edu.eg>
2021-08-12 03:09:15 +0200

    new Updates

--- resources/js/Views/Admin/RandomDistribution.vue
+++ resources/js/Views/Admin/RandomDistribution.vue
@@ -5,11 +5,50 @@
                 <h1 class="text-h4 font-weight-light">Random distribution</h1>
             </v-col>
         </v-row>
-        <v-row>
-            <p v-for="inv in data.invs" :key="inv.id">{{inv}}</p>
+        <v-row justify="center" v-if="users && users.length > 0">
+            <v-col cols="6" lg="3" md="3">
+                <v-card elevation="3" color="green lighten-4">
+                    <v-card-title class="justify-center">
+                        <h1 class="font-weight-light"><v-icon left>mdi-chevron-triple-down</v-icon> Min count</h1>
+                    </v-card-title>
+                    <v-card-text>
+                        <h1 class="text-h2 text-center">{{minUser().invs_count}}</h1>
+                    </v-card-text>
+                </v-card> 
+            </v-col>
+            <v-col cols="6" lg="3" md="">
+                <v-card elevation="3" dark color="red darken-4">
+                    <v-card-title class="justify-center">
+                        <h1 class="font-weight-light"><v-icon left>mdi-chevron-triple-up</v-icon> Max count</h1>
+                    </v-card-title>
+                    <v-card-text>
+                        <h1 style="color: white" class="text-h2 text-center">{{maxUser().invs_count}}</h1>
+                    </v-card-text>
+                </v-card> 
+            </v-col>
+            <v-col cols="6" lg="3" md="">
+                <v-card elevation="3" dark color="blue darken-1">
+                    <v-card-title class="justify-center">
+                        <h1 class="font-weight-light"><v-icon x-large>mdi-math-integral</v-icon> Average</h1>
+                    </v-card-title>
+                    <v-card-text>
+                        <h1 style="color: white" class="text-h2 text-center">{{getAvg().toFixed(2)}}</h1>
+                    </v-card-text>
+                </v-card> 
+            </v-col>
         </v-row>
         <v-row>
-            {{data.users}}
+            <v-col>
+                <v-data-table :headers="headers" :items="users">
+                    <template v-slot:[`item.index`]="{index}">
+                        {{index+1}}
+                    </template>
+                    <template v-slot:[`item.actions`]="{item}">
+                        <v-btn color="info" style="text-decoration: none" icon small :to="{name: 'userProfile', params:{id: item.id}}"><v-icon small>mdi-account</v-icon></v-btn>
+                        <v-btn style="text-decoration: none" class = "ml-2" icon small :to="{name: 'editUser', params:{id: item.id}}"><v-icon small>mdi-pencil</v-icon></v-btn>
+                    </template>
+                </v-data-table>
+            </v-col>
         </v-row>
     </v-container>
 </template>
@@ -18,7 +57,16 @@
 export default {
     data(){
         return{
-            data: ''
+            data: '',
+            users: [],
+            
+            headers: [
+                {text: '#', value: 'index'},
+                {text: 'name', value: 'name'},
+                {text: 'department', value: 'department.name'},
+                {text: 'invs count', value: 'invs_count'},
+                {text: 'actions', value: 'actions'}
+            ]
         }
     },
     mounted(){
@@ -31,10 +79,35 @@ export default {
         })
         .then((response) => {
             this.data = response.data
+            this.users = response.data.users
         })
         .catch((error) => {
             console.log(error.response.data)
         })
+    },
+    methods:{
+        minUser(){
+           let min = this.users[0];
+           $.each(this.users, (index, user) => {
+               min.invs_count < user.invs_count ? min = min : min = user
+           })
+           return min
+        },
+        maxUser(){
+            let max = this.users[0];
+            $.each(this.users, (index, user) => {
+                max.invs_count > user.invs_count ? max = max : max = user
+            })
+            return max
+        },
+        getAvg(){
+            let sum = 0 
+            $.each(this.users, (index, user) => {
+                sum += user.invs_count
+            })
+            let avg = sum / this.users.length
+            return avg
+        }
     }
     
 }
--- resources/js/Views/Admin/RoomProfile.vue
+++ resources/js/Views/Admin/RoomProfile.vue
@@ -2,6 +2,7 @@
     <v-container>
         <Loading :loading="loading" />
         <Alert @alert-closed="alert = false" :alert="alert" :alertMessage="alertMessage" :alertType="alertType" />
+        <Confirmation @dialog-closed="dialog = false" :confirmationText="dialogText" :dialog="dialog" :dialogData="dialogData" :onDeleteFunction="dialogFunction" />
         <v-row v-if="!loading && room">
             <v-col>
                 <h1 class="text-h4 font-weight-light">{{ room.number }}</h1>
@@ -22,8 +23,9 @@
                     <template v-slot:[`item.users_count`]="{item}">
                         {{item.users.length}}
                     </template>
-                    <template v-slot:[`item.users_limit`]>
-                        {{room.users_limit}}
+                    <template v-slot:[`item.actions`]="{item}">
+                        <v-btn style="text-decoration: none" icon small color="info" :to="{name: 'invProfile', params:{id: item.id}}"><v-icon small>mdi-door</v-icon></v-btn>
+                        <v-btn style="text-decoration: none" icon small color="error" class="ml-2" @click="confirm(item)"><v-icon small>mdi-delete</v-icon></v-btn>
                     </template>
                 </v-data-table>
             </v-col>
@@ -34,17 +36,20 @@
 <script>
 import Loading from '../../components/Loading.vue'
 import Alert from '../../components/Alert.vue'
+import Confirmation from '../../components/Confirmation.vue'
 export default {
     components: {
-        Loading, Alert
+        Loading, Alert, Confirmation
     },
     data(){
         return{
             room: Object,
             loading: true,
+
             alert: false,
             alertType: null,
             alertMessage: null,
+
             headers: [
                 {text: '#', value: 'index'},
                 {text: 'date', value: 'date'},
@@ -52,8 +57,14 @@ export default {
                 {text: 'users count', value: 'users_count'},
                 {text: 'users limit', value: 'users_limit'},
                 {text: 'course', value: 'course.code'},
-                {text: 'department', value: 'course.department.name'}
+                {text: 'department', value: 'course.department.name'},
+                {text: 'actions', value: 'actions'}
             ],
+
+            dialog: false,
+            dialogText: null,
+            dialogFunction: null,
+            dialogData: null
         }
     },
     mounted(){
@@ -75,6 +86,17 @@ export default {
             this.alertMessage = error.response.data.message
         })
 
+    },
+    methods:{
+        confirm(item){
+            this.dialog = true
+            this.dialogData = item.id
+            this.dialogText = "Are you sure you want to delete inv on " + this.$options.filters.DateFormat(item.date_time) + " ?"
+            this.dialogFunction = this.removeInv
+        },
+        removeInv(invId){
+
+        }
     },
     filters:{
             DateFormat(value)
