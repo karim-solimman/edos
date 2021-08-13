32136791691afabb3358915a57249f284c6cc4ee
Karim Soliman <karim.soliman@pua.edu.eg>
2021-08-14 00:39:11 +0200

    new Updates

---
+++ app/Imports/UsersImport.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
+
+class UsersImport implements ToModel, WithValidation, WithHeadingRow
+{
+    private $rows = 0;
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        ++$this->rows;
+        $dep_name = $row['department'];
+        $dep = DB::table('departments')->where('name', 'like', "%{$dep_name}%")->first();
+        return new User([
+            'name' => $row['name'],
+            'email' => $row['email'],
+            'department_id' => $dep->id,
+        ]);
+    }
+
+    public function rules(): array
+    {
+        return  [
+            'email' => ['required', 'email', 'unique:users,email'],
+        ];
+    }
+
+    public function getRowCount(): int
+    {
+        return $this->rows;
+    }
+}
