a5fc9c5d82f3eb0e7eb552c31b0a57d219829a69
Karim Soliman <karim.soliman@pua.edu.eg>
2021-08-17 02:52:06 +0200

    new Updates

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use App\Models\Department;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
@@ -36,6 +37,64 @@ class CourseController extends Controller
         $course->department_id = $request->input('department_id');
         $course->save();
         return response(['message' => $course->code.' - '.$course->name.' Added successfully']);
+    }
+
+    public function editInformation(Request $request)
+    {
+        $request->validate([
+           'course_id' => ['required', 'integer', 'exists:courses,id'],
+            'course_code' => ['required', 'string'],
+            'course_name' => ['required', 'string' , 'min:10'],
+            'course_credit_hours' => ['required', 'integer'],
+            'department_id' => ['required', 'integer', 'exists:departments,id']
+        ]);
+        $course = Course::where('id', $request->input('course_id'))->firstOrFail();
+        if(Course::where('code', strtoupper($request->input('course_code')))->get()->count() > 0 && $course->code != strtoupper($request->input('course_code')))
+        {
+            return response(['message' => $course->code.' Already Exists'],402);
+        }
+        $course->code = strtoupper($request->input('course_code'));
+        $course->name = $request->input('course_name');
+        $course->credit_hours = $request->input('course_credit_hours');
+        $course->department_id = $request->input('department_id');
+        $course->save();
+
+        return response(['message' => $course->code.' Updated successfully'],201);
+    }
+
+    public function updateDataTime(Request $request)
+    {
+        $request->validate([
+            'course_id' => ['required', 'integer', 'exists:courses,id'],
+            'date' => ['required', 'date_format:Y-m-d'],
+            'time' => ['required', 'date_format:H:i']
+        ]);
+        $date_time = $request->input('date').' '.$request->input('time');
+        $course = Course::with(['invs'])->where('id',$request->input('course_id'))->firstOrFail();
+        if (count($course->invs) <= 0 )
+        {
+            return response(['message' => $course->code.' Has no invs to be updated'],402);
+        }
+        $course->invs()->update(['date_time' => $date_time]);
+        return response(['message' => 'date and time updated to '.Carbon::parse($date_time)->toDateString().' at '.Carbon::parse($date_time)->format('H:i A')],201);
+    }
+
+    public function removeAllInvs(Request $request)
+    {
+        $request->validate([
+           'course_id' => ['required', 'integer', 'exists:courses,id']
+        ]);
+        $course = Course::with(['invs'])->where('id',$request->input('course_id'))->firstOrFail();
+        if (count($course->invs)>0)
+        {
+            foreach ($course->invs as $inv)
+            {
+                $inv->users()->detach();
+            }
+            $course->invs()->delete();
+        }
+
+        return response(['message' => $course->code.' All invs removed successfully'],201);
     }
 
     public function delete($id)
