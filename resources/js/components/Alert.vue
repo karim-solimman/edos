f95b6a7782fcda9f71807c8912d14567a8ffc921
Karim Soliman <karim.soliman@pua.edu.eg>
2021-08-19 21:31:43 +0200

    new Updates

--- resources/js/components/Alert.vue
+++ resources/js/components/Alert.vue
@@ -1,56 +1,61 @@
 <template>
-    <v-snackbar width="720" multi-line :timeout="timeout" v-model="value" elevation="3" :color="alertType">
+    <v-snackbar
+        width="720"
+        multi-line
+        :timeout="timeout"
+        v-model="value"
+        elevation="3"
+        :color="alertType"
+    >
         <v-row>
             <v-col class="my-auto" cols="1">
-                <v-icon left v-if="alertType == 'success'">mdi-check-circle-outline</v-icon>
+                <v-icon left v-if="alertType == 'success'"
+                    >mdi-check-circle-outline</v-icon
+                >
                 <v-icon left v-else>mdi-alert-circle-outline</v-icon>
             </v-col>
-            <v-col>
+            <v-col class="my-auto">
                 {{ alertMessage }}
             </v-col>
         </v-row>
-      <template v-slot:action="{ attrs }">
-        <v-btn
-          text
-          v-bind="attrs"
-          @click="close"
-        >
-        Close
-        </v-btn>
-      </template>
+        <template v-slot:action="{ attrs }">
+            <v-btn text v-bind="attrs" @click="close">
+                Close
+            </v-btn>
+        </template>
     </v-snackbar>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
+    data() {
+        return {
             timeout: 4000,
             value: this.alert
-        }
+        };
     },
-    props:{
+    props: {
         alert: Boolean,
         alertType: String,
         alertMessage: String
     },
-    methods:{
-        close(){
-            this.value = false
-            this.$emit('alert-closed')
+    methods: {
+        close() {
+            this.value = false;
+            this.$emit("alert-closed");
         }
     },
-    watch:{
-        alert: function (newVal, oldVal) {
-            if(newVal) {
-                this.value = newVal
+    watch: {
+        alert: function(newVal, oldVal) {
+            if (newVal) {
+                this.value = newVal;
             }
         },
-        value: function (newVal, oldVal) {
-            if(oldVal){
-                this.close()
-            }            
+        value: function(newVal, oldVal) {
+            if (oldVal) {
+                this.close();
+            }
         }
     }
-}
+};
 </script>
