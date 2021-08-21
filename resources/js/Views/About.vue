3b1fb7248739c2122ca26aad9328b39be56b6a43
Karim Soliman <karim.soliman@pua.edu.eg>
2021-08-22 01:10:16 +0200

    new Updates

--- resources/js/Views/About.vue
+++ resources/js/Views/About.vue
@@ -3,7 +3,9 @@
         <v-row>
             <v-col cols="12">
                 <h1 class="text-h1 font-weight-thin text-center">EDOS</h1>
-                <p class=" text-center text-overline">DISTRIBUTE, SWAP, MONITOR, AND EDIT</p>
+                <p class=" text-center text-overline">
+                    DISTRIBUTE, SWAP, MONITOR, AND EDIT
+                </p>
             </v-col>
         </v-row>
         <v-divider></v-divider>
@@ -16,10 +18,16 @@
             <v-col class="my-auto">
                 <h1 class="text-h5 font-weight-thin">About EDOS</h1>
                 <p class="text-body-1">
-                    EDOS, is a mini web application that was found to speed and enhance exams observers distribution process, it's quite simple and easy to use the existence techonlogy to make each step of the educational process quite simple and hassle free.
+                    EDOS, is a mini web application that was found to speed and
+                    enhance exams observers distribution process, it's quite
+                    simple and easy to use the existence techonlogy to make each
+                    step of the educational process quite simple and hassle
+                    free.
                 </p>
-                 <p class="text-body-1">
-                    Currently, the educational process envolve too much paper work and too many time consuming tasks that can be reduced by using user-friendly apps like EDOS.
+                <p class="text-body-1">
+                    Currently, the educational process envolve too much paper
+                    work and too many time consuming tasks that can be reduced
+                    by using user-friendly apps like EDOS.
                 </p>
             </v-col>
         </v-row>
@@ -27,31 +35,72 @@
             <v-col class="my-auto" cols="12" lg="6" md="6">
                 <h1 class="text-h5 font-weight-thin">EDOS for PUA</h1>
                 <p class="text-body-1">
-                    This app is specifically didcated to <strong>Pharos Univerity in Alexandria (PUA), Faculty of Engineering</strong> staff members, to help them finish the distribution task faster and in efficient way.
+                    This app is specifically didcated to
+                    <strong
+                        >Pharos Univerity in Alexandria (PUA), Faculty of
+                        Engineering</strong
+                    >
+                    staff members, to help them finish the distribution task
+                    faster and in efficient way.
                 </p>
                 <p class="text-body-1">
-                    At (PUA) TAs and staff members have other duties and responsibilties to do dialy other than teaching and instructing students, these tasks and duties are time consuming tasks. I developed EDOS voluntarily to Faculty of Engineering to be example of what current technology can do to make tasks and duties much easier.
+                    At (PUA) TAs and staff members have other duties and
+                    responsibilties to do dialy other than teaching and
+                    instructing students, these tasks and duties are time
+                    consuming tasks. I developed EDOS voluntarily to Faculty of
+                    Engineering to be example of what current technology can do
+                    to make tasks and duties much easier.
                 </p>
             </v-col>
             <v-col class="my-auto d-flex justify-center">
-                <v-img  max-height="200" max-width="200" src="/img/logo.png"></v-img>
+                <v-img
+                    max-height="200"
+                    max-width="200"
+                    src="/img/logo.png"
+                ></v-img>
             </v-col>
         </v-row>
         <v-row>
-            <v-col class="my-auto d-flex align-center justify-center flex-column">
-               <v-img max-height="200" max-width="200" class="rounded-circle transition-swing secondary" src="/img/soliman.jpg"></v-img>
-               <div class="mt-6">
-                    <v-btn style="text-decoration: none" href="https://www.facebook.com/karim.solimman/" color="primary" icon><v-icon>mdi-facebook</v-icon></v-btn>
-                    <v-btn style="text-decoration: none" href="https://www.linkedin.com/in/karim-soliman-24882253/" color="info" class="ml-5" icon><v-icon>mdi-linkedin</v-icon></v-btn>
+            <v-col
+                class="my-auto d-flex align-center justify-center flex-column"
+            >
+                <v-img
+                    max-height="200"
+                    max-width="200"
+                    class="rounded-circle transition-swing secondary"
+                    src="/img/soliman.jpg"
+                ></v-img>
+                <div class="mt-6">
+                    <v-btn
+                        style="text-decoration: none"
+                        href="https://www.facebook.com/karim.solimman/"
+                        color="primary"
+                        icon
+                        ><v-icon>mdi-facebook</v-icon></v-btn
+                    >
+                    <v-btn
+                        style="text-decoration: none"
+                        href="https://www.linkedin.com/in/karim-soliman-24882253/"
+                        color="info"
+                        class="ml-5"
+                        icon
+                        ><v-icon>mdi-linkedin</v-icon></v-btn
+                    >
                 </div>
             </v-col>
             <v-col>
                 <h1 class="text-h5 font-weight-thin">Karim Soliman</h1>
                 <p class="text-body-1">
-                    Karim Soliman is the developer behind EDOS, he was a PUA student for 5 years since 2009 and graduated 2014 with BSc in Computer Engineering.
-                    He finished his MSc in Computer Engineering field from Arab Academy for Science Technology & Maritime Transport (AASTMT) in 2018. 
-                    Currently he is studying PhD at Beijing Institute of Technology (BIT) at Beijing - China. He believes that learning is a non-stop process and everyone can or may learn more and more.
-                    Maybe i'm not a professional full-stack web developer but i'm trying to be better and learn more.
+                    Karim Soliman is the developer behind EDOS, he was a PUA
+                    student for 5 years since 2009 and graduated 2014 with BSc
+                    in Computer Engineering. He finished his MSc in Computer
+                    Engineering field from Arab Academy for Science Technology &
+                    Maritime Transport (AASTMT) in 2018. Currently he is
+                    studying PhD at Beijing Institute of Technology (BIT) at
+                    Beijing - China. He believes that learning is a non-stop
+                    process and everyone can or may learn more and more. Maybe
+                    i'm not a professional full-stack web developer but i'm
+                    trying to be better and learn more.
                 </p>
             </v-col>
         </v-row>
@@ -59,7 +108,5 @@
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
