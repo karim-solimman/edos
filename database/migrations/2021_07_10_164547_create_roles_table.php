ba77fd39cc70465a204a1034301f0e0b51c0efb1
Karim Soliman <karim.soliman@pua.edu.eg>
2021-08-21 17:40:05 +0200

    new Updates

--- database/migrations/2021_07_10_164547_create_roles_table.php
+++ database/migrations/2021_07_10_164547_create_roles_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateRolesTable extends Migration
@@ -19,6 +20,28 @@ class CreateRolesTable extends Migration
             $table->string('slug')->unique();
             $table->timestamps();
         });
+
+        DB::table('roles')->insert([
+           [
+               'name' => 'admin',
+               'slug' => 'admin',
+               'created_at' => now(),
+               'updated_at' => now()
+           ],
+            [
+                'name' => 'user',
+                'slug' => 'user',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'data entry',
+                'slug' => 'de',
+                'created_at' => now(),
+                'updated_at' => now()
+
+            ]
+        ]);
     }
 
     /**
